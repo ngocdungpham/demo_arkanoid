57c08de2bcff60930ec7b218e7a56e0384e08f6b
Vinh <24020364@vnu.edu.vn>
2025-10-16 15:19:26 +0700

    fix lỗi powerup còn sau khi sang levels

--- src/main/java/com/ooparkanoid/object/PowerUp/ShrinkPaddleEffect.java
+++ src/main/java/com/ooparkanoid/object/PowerUp/ShrinkPaddleEffect.java
@@ -1,6 +1,7 @@
 package com.ooparkanoid.object.PowerUp;
 
 import com.ooparkanoid.object.Paddle;
+import com.ooparkanoid.utils.Constants;
 
 public class ShrinkPaddleEffect implements PowerUpEffect {
     private final double scaleFactor;
@@ -14,7 +15,7 @@ public class ShrinkPaddleEffect implements PowerUpEffect {
     public void apply(GameContext context) {
         Paddle paddle = context.getPaddle();
         if (originalWidth < 0) {
-            originalWidth = paddle.getWidth();
+            originalWidth = Constants.PADDLE_WIDTH;
         }
         paddle.setWidth(originalWidth * scaleFactor);
         System.out.println("→← ShrinkPaddle effect applied!");
